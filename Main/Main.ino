a43101b43cf1e67ce6787f5e8d8cfb2579da8be1
St-H123 <hazels.05.remount@icloud.com>
2023-04-26 18:46:06 +0200

    made some header files I made some header files for the object that we could later use. I doupt if they will make it into final production though. I also searched for librarys to use with the sensors that we have in the arduino, but have not found them all.

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,39 +1,65 @@
-// Printing statements
-#define ARROW " ==> "
-#define BUILD ("[BUILD]" + ARROW)
-#define ERROR ("[ERROR]" + ARROW)
-#define MESSAGE ("[ MSG ]" + ARROW)
-
+/**
+ * @file This is the main file where the code starts execution. This is where the robot will  
+ * process the data from it's sensors, and steer itself away from craters and mountains to
+ * find the rocks on the surface.
+ *
+ * @author Tygo van den Hurk, studen at TU/e: 1705709
+ *
+ * @version 1.0
+ */
 // ---- ---- ---- ---- ---- ---- ---- ----  communications  ---- ---- ---- ---- ---- ---- ---- ----
-/** This pin is used for communications */
-#define ZIGBEE_ROUTER_PIN_RX 0  
-/** This pin is used for communications */
-#define ZIGBEE_ROUTER_PIN_TX 1  
+#include <XBee.h>
+
+/** This pin is used for communications, receiving in this case. */
+#define ZIGBEE_ROUTER_PIN_RX 0
+/** This pin is used for communications, sending in this case. */
+#define ZIGBEE_ROUTER_PIN_TX 1
 
 // ---- ---- ---- ---- ---- ---- ---- ---- -- Encoder's --- ---- ---- ---- ---- ---- ---- ---- ----
-/** I do not know what the encoder does */
+#include "Encoder.h"
+/** These keep track of how many rotations the left wheel makes. */
 #define LEFT_ENCODER 7
-/** I do not know what the encoder does */
+/** These keep track of how many rotations the right wheel makes. */
 #define RIGHT_ENCODER 8
 
 // ---- ---- ---- ---- ---- ---- ---- ---- --- Sensor's --- ---- ---- ---- ---- ---- ---- ---- ----
+#include "UltraSoundSensor.h"
+
 /** This pin is the ultraSoundSensor*/
 #define ULTRA_SOUND_SENSOR_PIN 9
+/** Object to get the distance from the robot to something in frond of it. */
+UltraSoundSensor soundSensor(ULTRA_SOUND_SENSOR_PIN);
 
-// ---- ---- ---- ---- ---- ---- ---- ---- --- Servo's ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+// ---- ---- ---- ---- ---- ---- ---- ---- --- Servo's ---- ---- ---- ---- ---- ---- ---- ---- ----
+#include "ServoMotor.h"
+
 /** This pin is used for the GrabberServo, that is attached to the grabber */
-#define GRABBER_SERVO_PIN 10 
+#define GRABBER_SERVO_PIN 10
+/** Object to control the grabber of the robot */
+ServoMotor grabber(GRABBER_SERVO_PIN);
+
 /** This pin is used for the UltraSoundServo, that is attached to the "face" */
-#define ULTRA_SOUND_SENSOR_PIN 11
+#define ULTRA_SOUND_SERVO_PIN 11
+/** Object to control rotation of the UltraSoundSensor */
+ServoMotor sensorServo(ULTRA_SOUND_SERVO_PIN);
+
 /** This pin is used for the LeftServo, that is attached to the left wheel */
 #define LEFT_SERVO_PIN 12
+/** Object to control left wheel of the robot */
+ServoMotor leftWheel(LEFT_SERVO_PIN);
+
 /** This pin is used for the LeftServo, that is attached to the right wheel */
 #define RIGHT_SERVO_PIN 13
+/** Object to control left wheel of the robot */
+ServoMotor rightWheel(RIGHT_SERVO_PIN);
 
+// ---- ---- ---- ---- ---- ---- ---- ---- Global Variables ---- ---- ---- ---- ---- ---- ---- ----
+
+// These must be at least a short since a byte only goes until 128, while a short goes until 32768.
 /** stores where the robot is on the map in respect to the lab. */
-int xCoordinateRobot = 0; 
+short xCoordinateRobot = 0;
 /** stores where the robot is on the map in respect to the lab. */
-int yCoordinateRobot = 0;
+short yCoordinateRobot = 0;
 
 // ---- ---- ---- ---- ---- ---- ---- ---- --- Method's --- ---- ---- ---- ---- ---- ---- ---- ----
 /**
@@ -41,13 +67,13 @@ int yCoordinateRobot = 0;
  */
 void setup() {
   Serial.begin(9600);
-  Serial.println(BUILD + "try setting up.");
-} 
+  Serial.println("try setting up.");
+}
 
 /**
- * @brief This is where we loop until our objective is 
+ * @brief This is where we loop until our objective is completed.
  */
 void loop() {
-  Serial.print(MESSAGE + "hello world!");
-  sleep(100);
+  Serial.print("hello world!");
+  delay(1000);
 }
