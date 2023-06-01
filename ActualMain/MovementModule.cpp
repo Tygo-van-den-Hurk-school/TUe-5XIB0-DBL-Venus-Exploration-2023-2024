8cb28580667a5e4a9859ef3bdc7040f492c78753
St-H123 <hazels.05.remount@icloud.com>
2023-06-01 15:14:18 +0200

    added comments

--- ActualMain/MovementModule.cpp
+++ ActualMain/MovementModule.cpp
@@ -13,58 +13,66 @@ Movement::Movement(){
 
 void Movement::moveForward(const int SPEED){
 
-  const bool SPEED_IS_TOO_HIGH = (SPEED > 499);
-  const bool SPEED_IS_TOO_LOW = (SPEED < 0);
-  const bool INVALID_SPEED = (SPEED_IS_TOO_HIGH || SPEED_IS_TOO_LOW);
-  if (INVALID_SPEED) {
-    Serial.println("FIS");
-    return;
+  /* validating the Pre-Conditions */ {
+    const bool SPEED_IS_TOO_HIGH = (SPEED > 499);
+    const bool SPEED_IS_TOO_LOW = (SPEED < 0);
+    const bool INVALID_SPEED = (SPEED_IS_TOO_HIGH || SPEED_IS_TOO_LOW);
+    if (INVALID_SPEED) {
+      Serial.println("FIS");
+      return;
+    }
+
+    const bool WAS_ALREADY_MOVING = (this->movementDirection == FORWARDS);
+    if (WAS_ALREADY_MOVING) {
+      Serial.println("FIM");
+      return;
+    }
   }
 
-  const bool WAS_ALREADY_MOVING = (this->movementDirection == FORWARDS);
-  if (WAS_ALREADY_MOVING) {
-    Serial.println("FIM");
-    return;
+  /* Made the motors go forwards with the speed */ {
+    Serial.println("Fm");
+    this->leftWheel.writeMicroseconds(NEUTRAL + (FORWARDS * SPEED));
+    this->rightWheel.writeMicroseconds(NEUTRAL - (FORWARDS * SPEED));
+    this->movementDirection = FORWARDS;
   }
-
-  Serial.println("Fm");
-
-  this->leftWheel.writeMicroseconds(NEUTRAL + (FORWARDS * SPEED));
-  this->rightWheel.writeMicroseconds(NEUTRAL - (FORWARDS * SPEED));
-  this->movementDirection = FORWARDS;
 }
 
 void Movement::moveBackward(const int SPEED){
 
-  const bool SPEED_IS_TOO_HIGH = (SPEED > 499);
-  const bool SPEED_IS_TOO_LOW = (SPEED < 0);
-  const bool INVALID_SPEED = (SPEED_IS_TOO_HIGH || SPEED_IS_TOO_LOW);
-  if (INVALID_SPEED) {
-    Serial.println("BIS");
-    return;
+  /* validating the Pre-Conditions */ {
+    const bool SPEED_IS_TOO_HIGH = (SPEED > 499);
+    const bool SPEED_IS_TOO_LOW = (SPEED < 0);
+    const bool INVALID_SPEED = (SPEED_IS_TOO_HIGH || SPEED_IS_TOO_LOW);
+    if (INVALID_SPEED) {
+      Serial.println("BIS");
+      return;
+    }
+
+    const bool WAS_ALREADY_MOVING = (this->movementDirection == BACKWARDS);
+    if (WAS_ALREADY_MOVING) {
+      Serial.println("BIM");
+      return;
+    }
   }
 
-  const bool WAS_ALREADY_MOVING = (this->movementDirection == BACKWARDS);
-  if (WAS_ALREADY_MOVING) {
-    Serial.println("BIM");
-    return;
+  /* Made the motors go backwards with the speed */ {
+    Serial.println("Bm");
+    this->leftWheel.writeMicroseconds(NEUTRAL - (BACKWARDS * SPEED));
+    this->rightWheel.writeMicroseconds(NEUTRAL - (BACKWARDS * SPEED));
+    this->movementDirection = BACKWARDS;
   }
-
-  Serial.println("Bm");
-
-  this->leftWheel.writeMicroseconds(NEUTRAL - (BACKWARDS * SPEED));
-  this->rightWheel.writeMicroseconds(NEUTRAL - (BACKWARDS * SPEED));
-  this->movementDirection = BACKWARDS;
 }
 
 void Movement::turn(const float ANGLE){
 
-  const bool ANGLE_IS_TOO_HIGH = (ANGLE > 360);
-  const bool ANGLE_IS_TOO_LOW = (ANGLE < 0);
-  const bool INVALID_ANGLE = (ANGLE_IS_TOO_HIGH || ANGLE_IS_TOO_LOW);
-  if (INVALID_ANGLE) {
-    Serial.println("TIA");
-    return;
+  /* validating the Pre-Conditions */ {
+    const bool ANGLE_IS_TOO_HIGH = (ANGLE > 360);
+    const bool ANGLE_IS_TOO_LOW = (ANGLE < 0);
+    const bool INVALID_ANGLE = (ANGLE_IS_TOO_HIGH || ANGLE_IS_TOO_LOW);
+    if (INVALID_ANGLE) {
+      Serial.println("TIA");
+      return;
+    }
   }
 
   Serial.println("Tm");
@@ -73,15 +81,18 @@ void Movement::turn(const float ANGLE){
 
 void Movement::stop(){
 
-  const bool WAS_NOT_EVEN_MOVING = (this->movementDirection == NOT_MOVING);
-  if (WAS_NOT_EVEN_MOVING) {
-    Serial.println("SIM");
-    return;
+  /* validating the Pre-Conditions */ {
+    const bool WAS_NOT_EVEN_MOVING = (this->movementDirection == NOT_MOVING);
+    if (WAS_NOT_EVEN_MOVING) {
+      Serial.println("SIM");
+      return;
+    }
   }
 
-  Serial.println("Sm");
-
-  this->leftWheel.writeMicroseconds(NEUTRAL);
-  this->rightWheel.writeMicroseconds(NEUTRAL);
-  this->movementDirection = NOT_MOVING;
+  /* Stopping the wheels */ {
+    Serial.println("Sm");
+    this->leftWheel.writeMicroseconds(NEUTRAL);
+    this->rightWheel.writeMicroseconds(NEUTRAL);
+    this->movementDirection = NOT_MOVING;
+  }
 }
