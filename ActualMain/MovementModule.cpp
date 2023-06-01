3e53b6c7d1b8351fe95c7eda336af5e65ad20818
St-H123 <hazels.05.remount@icloud.com>
2023-06-01 14:43:19 +0200

    found out what pins do what

--- ActualMain/MovementModule.cpp
+++ ActualMain/MovementModule.cpp
@@ -1,10 +1,14 @@
 #include "MovementModule.h"
 
 Movement::Movement(){
+  
   this->coordinates.x = 0.0;
   this->coordinates.y = 0.0;
+
   this->leftWheel.attach(LEFT_MOTOR_PIN);
   this->rightWheel.attach(RIGHT_MOTOR_PIN); 
+
+  this->stop();
 }
 
 void Movement::moveForward(const int SPEED){
@@ -13,11 +17,21 @@ void Movement::moveForward(const int SPEED){
   const bool SPEED_IS_TOO_LOW = (SPEED < 0);
   const bool INVALID_SPEED = (SPEED_IS_TOO_HIGH || SPEED_IS_TOO_LOW);
   if (INVALID_SPEED) {
+    Serial.println("FIS");
     return;
   }
 
-  this->leftWheel.writeMicroseconds(NEUTRAL + (FORWARD * SPEED));
-  this->rightWheel.writeMicroseconds(NEUTRAL - (FORWARD * SPEED));
+  const bool WAS_ALREADY_MOVING = (this->movementDirection == FORWARDS);
+  if (WAS_ALREADY_MOVING) {
+    Serial.println("FIM");
+    return
+  }
+
+  Serial.println("Fm");
+
+  this->leftWheel.writeMicroseconds(NEUTRAL + (FORWARDS * SPEED));
+  this->rightWheel.writeMicroseconds(NEUTRAL - (FORWARDS * SPEED));
+  this->movementDirection = FORWARD;
 }
 
 void Movement::moveBackward(const int SPEED){
@@ -26,11 +40,21 @@ void Movement::moveBackward(const int SPEED){
   const bool SPEED_IS_TOO_LOW = (SPEED < 0);
   const bool INVALID_SPEED = (SPEED_IS_TOO_HIGH || SPEED_IS_TOO_LOW);
   if (INVALID_SPEED) {
+    Serial.println("BIS");
     return;
   }
 
-  this->leftWheel.writeMicroseconds(NEUTRAL + SPEED);
-  this->rightWheel.writeMicroseconds(NEUTRAL - SPEED);
+  const bool WAS_ALREADY_MOVING = (this->movementDirection == BACKWARDS);
+  if (WAS_ALREADY_MOVING) {
+    Serial.println("BIM");
+    return
+  }
+
+  Serial.println("Bm");
+
+  this->leftWheel.writeMicroseconds(NEUTRAL - (FORWARD * SPEED));
+  this->rightWheel.writeMicroseconds(NEUTRAL - (FORWARD * SPEED));
+  this->movementDirection = BACKWARDS;
 }
 
 void Movement::turn(const float ANGLE){
@@ -39,12 +63,25 @@ void Movement::turn(const float ANGLE){
   const bool ANGLE_IS_TOO_LOW = (ANGLE < 0);
   const bool INVALID_ANGLE = (ANGLE_IS_TOO_HIGH || ANGLE_IS_TOO_LOW);
   if (INVALID_ANGLE) {
+    Serial.println("TIA");
     return;
   }
+
+  Serial.println("Tm");
 
 }
 
 void Movement::stop(){
+
+  const bool WAS_NOT_EVEN_MOVING = (this->movementDirection == NOT_MOVING);
+  if (WAS_NOT_EVEN_MOVING) {
+    Serial.println("SIM");
+    return;
+  }
+
+  Serial.println("Sm");
+
   this->leftWheel.writeMicroseconds(NEUTRAL);
   this->rightWheel.writeMicroseconds(NEUTRAL);
+  this->movementDirection = NOT_MOVING;
 }
--- ActualMain/MovementModule.h
+++ ActualMain/MovementModule.h
@@ -3,27 +3,70 @@
 // 11 is the head
 #define LEFT_MOTOR_PIN 12
 #define RIGHT_MOTOR_PIN 13
-#define FORWARD -1
+
+#define FORWARDS    -1
+#define NOT_MOVING  0
+#define BACKWARDS   1
+
 #define NEUTRAL 1500
 
-struct Coordinates
-{
-  float x,y;
+struct Coordinates {
+  float x, y;
 };
 
-class Movement
-{
+/**
+ * @class this class is used to control the robot's movement.
+ * @author Tygo van den Hurk, student at TU/e (1705709)
+ * @author David Constantin, student at TU/e (1569139)
+ * @version 1.0
+ */
+class Movement {
+
   private:
-    Servo leftWheel;
-    Servo rightWheel;
+    /** Stores the current coordinates of the robot. */
     Coordinates coordinates;
-    bool isMoving;
-    float angle;
+
+    /** The servo to which it controles the left wheel. */
+    Servo leftWheel;
+    /** The servo to which it controles the right wheel. */
+    Servo rightWheel;
+
+    /** 
+     * Keeps track of if the robot is in motion. is equal to FOWARD if going 
+     * forward, is equal to BACKWARD if going backwards, and is equal to 
+     * NOT_MOVING if not in motion.
+     */
+    byte movementDirection = NOT_MOVING;
+
+    /** Stores the angle with which the robot is moving */
+    float angle = 0;
 
   public:
+
+    /**
+     * @brief constructs a new Movement Object.
+     * @since 1.0
+     */
     Movement();
+
+    /**
+     * @brief Moves the robot forward indefinitely with a certain speed.
+     * @pre SPEED < 499 && SPEED > 0
+     * @param (int) SPEED the speed that with which the robot should move.
+     * @post this->movementDirection == FORWARD
+     * @since 1.0
+     */
     void moveForward(int);
+
+    /**
+     * @brief Moves the robot backwards indefinitely with a certain speed.
+     * @pre SPEED < 499 && SPEED > 0
+     * @param (int) SPEED the speed that with which the robot should move.
+     * @post this->movementDirection == BACKWARDS
+     * @since 1.0
+     */
     void moveBackward(int);
+
     void turn(float);
     void stop();
 };
