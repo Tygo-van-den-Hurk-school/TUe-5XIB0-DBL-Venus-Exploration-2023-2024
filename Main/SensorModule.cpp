d185acb817e090b58204b5cd3795e2bf793572d2
Tygo van den Hurk <hazels.05.remount@icloud.com>
2023-06-14 17:12:25 +0200

    Quality-control on SensorModule.cpp
    added TODO's for what still needs to be done, and should be updated.

--- Main/SensorModule.cpp
+++ Main/SensorModule.cpp
@@ -1,51 +1,70 @@
 #include "SensorModule.h"
 
 Sensors::Sensors() { 
-  // Initialize compass
-  compass.init();
-  compass.setSamplingRate(50); // Adjust the sampling rate as needed
-  compass.setRange(QMC5883L_RANGE_8GA); // Adjust the range as needed
-
-  sonar = NewPing(trigPin, echoPin);
-}
-
-Sensors::~Sensors(){
+  /* Initialize compass */ {
+    compass.init();
+    compass.setSamplingRate(50); // Adjust the sampling rate as needed
+    compass.setRange(QMC5883L_RANGE_8GA); // Adjust the range as needed
+  }
   
+  /* Initialize sonar */ {
+    sonar = NewPing(trigPin, echoPin);
+  }
 }
 
-bool Sensors::ambientLight(){
-    int ambientLightValue = analogRead(GPIOPinAmbient);
-    return ambientLightValue < 50;
+bool Sensors::ambientLight() { 
+    const int VALUE = (50); // TODO change the name of this variable to better reflect its purpose
+    const int AMBIENT_LIGHT = analogRead(GPIOPinAmbient);
+    
+    const bool LIGHT_WAS_NOT_BRIGHT_ENOUGH = (AMBIENT_LIGHT < VALUE); // TODO change the name of this variable to better reflect its purpose
+    return (LIGHT_WAS_NOT_BRIGHT_ENOUGH); // TODO change the name of this variable to better reflect its purpose
 }
 
-// Function to detect rocks
-bool Sensors::detectRocks() {
-  int infraredValue = analogRead(infraredPin);
-  return infraredValue > infraredThreshold;
+
+bool Sensors::rockDetected() {
+  const int MESSURED_INFRARED_VALUE = analogRead(infraredPin);
+  const bool ROCK_DETECTED = (MESSURED_INFRARED_VALUE > infraredThreshold);
+  return ROCK_DETECTED;
 }
 
-// Function to detect black tape boundaries
-bool Sensors::detectBoundaries() {
-  int leftLightValue = analogRead(leftLightPin);
-  int rightLightValue = analogRead(rightLightPin);
-  return leftLightValue > lightThreshold && rightLightValue > lightThreshold;
+bool Sensors::boundaryDetected() {
+  const int LEFT_LIGHT_VALUE = analogRead(leftLightPin);
+  const int RIGHT_LIGHT_VALUE = analogRead(rightLightPin);
+  
+  const bool LEFT_SUSPECTS_BOUNDRY = (LEFT_LIGHT_VALUE > lightThreshold);
+  const bool RIGHT_SUSPECTS_BOUNDRY = (RIGHT_LIGHT_VALUE > lightThreshold);
+  
+  const bool BOUNDRY_DETECTED = (LEFT_SUSPECTS_BOUNDRY && RIGHT_SUSPECTS_BOUNDRY); 
+  return BOUNDRY_DETECTED;
 }
 
-// Function to detect cliffs
-bool Sensors::detectCliffs() {
-  long distance = sonar.ping_cm();
-  return distance > 0 && distance <= 10;
+bool Sensors::cliffDetected() {
+  const long MINIMUM = (0); // TODO change the name of this variable to better reflect its purpose
+  const long MAXIMUM = (10); // TODO change the name of this variable to better reflect its purpose
+  const long DISTANCE = sonar.ping_cm();
+  
+  return ((DISTANCE > MINIMUM) && (DISTANCE <= MAXIMUM)); // TODO break this down into separate boolean statements, that explain what this range means
 }
 
-// Function to detect hills
-bool Sensors::detectHills() {
-  long distance = sonar.ping_cm();
-  return distance > 10 && distance <= 20;
+bool Sensors::hillDetected() {
+  const long MINIMUM = (10); // TODO change the name of this variable to better reflect its purpose
+  const long MAXIMUM = (20); // TODO change the name of this variable to better reflect its purpose
+  const long DISTANCE = sonar.ping_cm();
+  
+  const bool DISTANCE_IS_BELOW_MAXIMUM = (DISTANCE <= MAXIMUM); // TODO change the name of this variable to better reflect its purpose
+  const bool DISTANCE_IS_ABOVE_MINIMUM = (DISTANCE > MINIMUM); // TODO change the name of this variable to better reflect its purpose
+  
+  const bool DISTANCE_IS_IN_RANGE = (DISTANCE_IS_BELOW_MAXIMUM && DISTANCE_IS_ABOVE_MINIMUM);
+  return (DISTANCE_IS_IN_RANGE); 
 }
 
-// Function to read compass data
-float Sensors::readCompass() {
+float Sensors::getCompassHeading() {
   compass.read();
-  float heading = compass.getAzimuth();
-  return heading;
+  const float HEADING = compass.getAzimuth();
+  return HEADING;
 }
+
+// Sensors::~Sensors(){
+// If it does nothing, do not write it. It is just extra lines.
+// for now I have dumped it here
+// }
