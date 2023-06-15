601ed8bd8183963edbc69c916c203ae34b5d45e5
Tygo van den Hurk <hazels.05.remount@icloud.com>
2023-06-15 14:56:28 +0200

    Quality-control on Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,69 +1,202 @@
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ ~~ Libraries ~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
 #include "MovementModule.h"
 #include "SensorModule.h"
 #include "CommunicationModule.h"
-#include <Arduino.h>
-#include <ESP32Servo.h>
+#include <Arduino.h>                // TODO awnser this question: Why is this in here? Arduino includes this by default.
+#include <ESP32Servo.h>             // TODO awnser this question: were we not using "Servo.h"? is that less buggy?
 
-#define HEAD_PIN 25
-#define CLAWS_PIN 26
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ ~~ Constants ~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+/** The GPIO pin used for controling the head servo. */
+#define HEAD_SERVO_GPIO_PIN   25
+/** The GPIO pin used for controling the claw servo. */
+#define CLAWS_SERVO_GPIO_PIN  26
 
-Movement movement;
-Sensors sensors;
-Communication communication;
+/** this stores how many degrees zero-degrees are. */
+#define ZERO_DEGREES          0
+/** this stores how many degrees ninety-degrees are. */
+#define NINETY_DEGREES        90
+/** this stores how many degrees 180-degrees are. */
+#define ONE_EIGHTY_DEGREES    180
 
+/** the default delay time. */
+#define DEFAULT_DELAY_TIME    20
+/** the enum for when a situation is not understood/reconised. */
+#define NOT_UNDERSTOOD        0
+/** the enum for when a situation is that there are rocks detected. */
+#define ROCK_IS_DETECTED      1
+/** the enum for when a situation is that there are hills detected. */
+#define HILL_IS_DETECTED      2
+/** the enum for when a situation is that there are cliffs detected. */
+#define CLIFF_IS_DETECTED     3
+/** the enum for when a situation is that there are boundaries detected. */
+#define BOUNDARY_IS_DETECTED  4
 
-// General behaviour (head & claws)
-Servo servoHead;
-Servo servoClaws;
+/** the speed at which the Serial monitor parses the incoming bits. */
+#define DEFAULT_SERIAL_SPEED  115200
 
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ ~~ Objects ~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+/** The object that is responsible for all the movement of the robot. */
+const Movement movement;            // TODO remove this when the functions become static.
+/** The object that is responsible processing all the sensor data of the robot. */
+const Sensors sensors;              // TODO remove this when the functions become static.
+/** The object that is responsible for all the communications of the robot. */
+const Communication communication;  // TODO remove this when the functions become static.
 
-void clawsTurn(){
-  servoClaws.attach(CLAWS_PIN);
-  for(int posDegrees = 0; posDegrees <= 90; posDegrees++) {
-    servoClaws.write(posDegrees);
-    delay(20);
-  } for(int posDegrees = 90; posDegrees >= 20; posDegrees--) {
-    servoClaws.write(posDegrees);
-    delay(1);
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ Arduino main-functions ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+/**
+ * this method is the main method where the code starts running. This method is only run once. After this, the method `loop()`
+ * is run indefinitely. Meaning that that one will run until reset, or powerloss.
+ */
+void setup() {
+  /* Setting up Serial */ {
+    Serial.begin(DEFAULT_SERIAL_SPEED); 
   }
+  
+  /* creating the objects */ {                          // TODO remove this when the functions become static.
+    movement = Movement();                              // TODO remove this when the functions become static.
+    sensors = Sensors();                                // TODO remove this when the functions become static.
+    communication = Communication();                    // TODO remove this when the functions become static.
+  }
+  
+  /* Testing Communications? */ {                       // TODO awnser this question: Why is this in here?
+    strcpy(communication.myData.status, "something");   // TODO awnser this question: Why is this in here?
+    communication.send(communication.myData);           // TODO awnser this question: Why is this in here?
+  }
+}
+
+/**
+ * Loops until reset, or powerloss. This function fetched the current situation that the robot is in, and 
+ * for each case does the logic that belongs to that case.
+ */
+void loop() {
+  const int CURRENT_SITUATION = (getSituation());
+  
+  /* doing the logic for each situation. */ {
+    switch (CURRENT_SITUATION) {
+      case ROCK_IS_DETECTED:
+        // TODO put logic here
+        break;
+      case HILL_IS_DETECTED:
+        // TODO put logic here
+        break;
+      case CLIFF_IS_DETECTED:
+        // TODO put logic here
+        break;
+      case BOUNDARY_IS_DETECTED:
+        // TODO put logic here
+        break;
+      default:
+      Serial.print("[ ERR ] ==> Situation not reconised: ");
+      Serial.print(CURRENT_SITUATION);
+      Serial.print(".\n");
+    }
+  }
+
+  delay(DEFAULT_DELAY_TIME);
+}
+
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ ~~ Helper-functions ~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+/**
+ * Finds in what situation the robot is in, and returns that.
+ *
+ * @return and int that represents the situation that the robot is in.
+ */
+int getSituation() { // TODO move this function into its own file
+  
+  /* checking if a rock is detected */ {
+    const bool ROCK_DETECTED = (sensors.rockDetected());
+    if (ROCK_IS_DETECTED) {
+      return ROCK_IS_DETECTED;
+    }
+  }
+ 
+  /* checking if a hill is detected */ {
+    const bool HILL_DETECTED = (sensors.hillDetected);
+    if (HILL_IS_DETECTED) {
+      return HILL_IS_DETECTED;
+    }
+  }
+  
+  /* checking if a cliff is detected */ {
+    const bool CLIFF_DETECTED = (sensors.cliffDetected);
+    if (CLIFF_DETECTED) {
+      return CLIFF_IS_DETECTED;
+    }
+  }
+  
+  /* checking if a boundary is detected */ {
+    const bool BOUNDARY_DETECTED = (sensor.boundaryDetected());
+    if (BOUNDARY_DETECTED) {
+      return BOUNDARY_IS_DETECTED;
+    }
+  }
+  
+  return NOT_UNDERSTOOD;
+}
+
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  should-not-be-here-functions ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+/** The servo that controls the head of the robot. */
+const Servo servoHead;              // TODO awnser this question: Why is this in here in this file?
+/** The servo that controls the claws of the robot. */
+const Servo servoClaws;             // TODO awnser this question: Why is this in here in this file?
+
+/**
+ * Opens and closes the claws.
+ */
+void clawsTurn(){ // TODO awnser this question: Why is this in here in this file?
+  servoClaws.attach(CLAWS_SERVO_GPIO_PIN);
+  
+  /* Opening Claws */ {
+    for(int posDegrees = ZERO_DEGREES; posDegrees <= NINETY_DEGREES; posDegrees++) {
+      servoClaws.write(posDegrees);
+      delay(DEFAULT_DELAY_TIME);
+    }
+    printf("[LOG]: opened claw\n");
+  }
+  
+  /* Closing Claws */ {
+    for(int posDegrees = NINETY_DEGREES; posDegrees > ZERO_DEGREES; posDegrees--) {
+      servoClaws.write(posDegrees);
+      delay(DEFAULT_DELAY_TIME);
+    }
+    printf("[LOG]: closed claw\n");
+  }
+
   servoClaws.detach();
 }
 
-void headTurn(){
-  servoHead.attach(HEAD_PIN);
-  // if something detected at x angle store that angle
-  for(int posDegrees = 0; posDegrees <= 90; posDegrees++) {
-    servoHead.write(posDegrees);
-    delay(20);
+/**
+ * Turns the head of the robot left and right. Does nothing else, just turns.
+ * 
+ * TODO awnser this question: what is the purpose of this method?
+ */
+void headTurn(){ // TODO awnser this question: Why is this in here in this file?
+  servoHead.attach(HEAD_SERVO_GPIO_PIN);
+  
+  // if something detected at x angle store that angle // TODO awnser this question: is this a TODO?
+  
+  /* Turning head left */ {
+    for(int posDegrees = ZERO_DEGREES; posDegrees <= NINETY_DEGREES; posDegrees++) {
+      servoHead.write(posDegrees);
+      delay(DEFAULT_DELAY_TIME);
+    }
+    printf("[LOG]: Head turned left\n");
   }
-  printf("[LOG]: Head turned left\n");
+  
+  /* Turning head Right */ {
+    for(int posDegrees = ONE_EIGHTY_DEGREES; posDegrees >= ZERO_DEGREES; posDegrees--) {
+      servoHead.write(posDegrees);
+      delay(DEFAULT_DELAY_TIME);
+    }
+    printf("[LOG]: Head turned right\n");
+  }
 
-  for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
-    servoHead.write(posDegrees);
-    delay(20);
+  /* putting head back in original position */ {
+    for(int posDegrees = ZERO_DEGREES; posDegrees <= NINETY_DEGREES; posDegrees++) {
+      servoHead.write(posDegrees);
+      delay(DEFAULT_DELAY_TIME);
+    }
   }
-  printf("[LOG]: Head turned right\n");
-
-  for(int posDegrees = 0; posDegrees <= 90; posDegrees++) {
-    servoHead.write(posDegrees);
-    delay(1);
-  }
+  
   servoHead.detach();
-}
-
-
-void setup() {
-  Serial.begin(115200); // Always the first line in setupe is used to setup communication
-
-  movement = Movement();
-  sensors = Sensors();
-  communication = Communication();
-  strcpy(communication.myData.status,"something");
-  communication.send(communication.myData);
-
-}
-
-void loop() {
-
-    
 }
