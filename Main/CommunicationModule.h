64cf9de520266c23e0cf39468a16e81433b81806
Tygo van den Hurk <hazels.05.remount@icloud.com>
2023-06-15 15:13:31 +0200

    Quality-control on CommunicationModule.h

--- Main/CommunicationModule.h
+++ Main/CommunicationModule.h
@@ -1,31 +1,109 @@
 #pragma once
+
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ ~~ Libraries ~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
 #include "esp_now.h"
 #include "WiFi.h"
 #include <cstring>
+
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ ~~ Constants ~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
 #define BOARD true
 
-
-//Structure for sending messages --> MUST BE THE SAME FOR THE RECEIVER
-typedef struct sendMessage{
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ Data-Structures ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+/**
+ * Structure for sending messages 
+ * TODO MUST BE THE SAME FOR THE RECEIVER
+ */
+typedef struct sendMessage {
   char status[32];
 } sendMessage;
 
-class Communication
-{
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  ~~~~ ~~~~ Class ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+/**
+ * @class A class that handles the communication between the robots.
+ * @author Tygo van den Hurk, student at TU/e (1705709)
+ * @author David Constantin, student at TU/e (1569139)
+ * // TODO Other people, add your names, and student numbers in the same way, do not remove this comment until we hand this in
+ * @version 1.0
+ */
+class Communication {
+  // ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~ Constructors, and Destructors ~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~  
+  public:
+    /**
+     * Creates a new `Communication` Object.
+     */
+    Communication();
 
-    public:
-        Communication();
-        ~Communication();
-        int send(sendMessage);
-        void onReceive(const uint8_t * mac, const uint8_t * incomingData, int len);
-        sendMessage myData;
+    /**
+     * Deconstructs the `Communication` Object.
+     */
+    ~Communication();
 
-    private:
-        void receiverSetup();
-        void senderSetup();
-        void onDataSent(const uint8_t *, esp_now_send_status_t);
-        esp_now_peer_info_t peerInfo;
-        uint8_t board[8];
-        int errorCode = 0;
+    // ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~ Methods for sending and receiving data ~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+    /**
+     * TODO Discribe what the function does.
+     *
+     * @param (sendMessage) VARIABLE_NAME Discribe it's use case.
+     * @return Discribe what it returns, since it returns an int.
+     */
+    int send(sendMessage);
+  
+    /**
+     * TODO Discribe what the function does.
+     *
+     * @param (uint8_t) MAC Discribe it's use case.
+     * @param (uint8_t) INCOMMING_DATA Discribe it's use case.
+     * @param (int) LENGTH Discribe it's use case.
+     */
+    void onReceive(const uint8_t * MAC, const uint8_t * INCOMMING_DATA, const int LENGTH);
+    sendMessage myData;
 
+    // TODO create this function
+    /* *
+     * Prints a message over serial to the host.
+     *
+     * @pre ``
+     * @param (char[]) MESSAGE the message you want to sent over Serial
+     * /
+    void printSerial(const char[] MESSAGE, const unsigned int MESSAGE_LENGTH) {
+
+      /* Printing the timeStamp * / {
+        Serial.print("["+System.millis()+"] : ");
+      }
+
+      /* Printing the message * / {
+        for (int index = 0; index < MESSAGE_LENGTH; index++) {
+          Serial.print(MESSAGE[index]);
+        }
+      }
+    }; */
+
+  private:
+    // ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~ Methods for setting up comunications ~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
+    /**
+     * TODO Discribe what the function does.
+     */
+    void receiverSetup();
+
+    /**
+     * TODO Discribe what the function does.
+     */
+    void senderSetup();
+
+    // ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ Methods with unknown use cases ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ // TODO fill in what this section is about
+    /**
+     * TODO Discribe what the function does.
+     *
+     * @param (uint8_t) VARIABLE_NAME Discribe it's use case.
+     */
+    void onDataSent(const uint8_t *, esp_now_send_status_t);
+
+    // ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ Methods with unknown use cases ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ // TODO fill in what this section is about
+    /** TODO Discribe it's use case. */
+    esp_now_peer_info_t peerInfo;
+
+    /** TODO Discribe it's use case. */
+    uint8_t board[8];
+
+    /** TODO Discribe it's use case. */
+    int errorCode = 0; // TODO awnser quesion, why do we need this?
 };
