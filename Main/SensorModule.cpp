f6334c9b402abda1fbaa28c35fdc1b9e51a88668
Tygo van den Hurk <hazels.05.remount@icloud.com>
2023-06-15 12:06:30 +0200

    Quality-control on SensorModule.cpp

--- Main/SensorModule.cpp
+++ Main/SensorModule.cpp
@@ -1,70 +1,82 @@
 #include "SensorModule.h"
 
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~ Constructors, and Destructors ~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~
 Sensors::Sensors() { 
-  /* Initialize compass */ {
-    compass.init();
-    compass.setSamplingRate(50); // Adjust the sampling rate as needed
-    compass.setRange(QMC5883L_RANGE_8GA); // Adjust the range as needed
+  /* Initialize the compass */ {
+    this -> COMPASS.init();
+    this -> COMPASS.setSamplingRate(SAMPLING_RATE_COMPASS);
+    this -> COMPASS.setRange(QMC5883L_RANGE_8GA); // Adjust the range as needed
   }
   
-  /* Initialize sonar */ {
-    sonar = NewPing(trigPin, echoPin);
+  /* Initialize the sonar */ {
+    this -> SONAR = NewPing(trigPin, echoPin);
   }
 }
 
+// Sensors::~Sensors(){
+// If it does nothing, do not write it. It is just extra lines. 
+// Plus I do not expect an arduino to destruct objects when shut
+// down due to power loss. So is this even needed?
+// }
+
+// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ Sensor value returning methods ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ 
 bool Sensors::ambientLight() { 
     const int VALUE = (50); // TODO change the name of this variable to better reflect its purpose
-    const int AMBIENT_LIGHT = analogRead(GPIOPinAmbient);
-    
+    const int AMBIENT_LIGHT = analogRead(AMBIENT_LIGHT_SENSOR_GPIO_PIN);
+    // TODO Awnser this question: why are we not using "LIGHT_THRESHOLD"?
     const bool LIGHT_WAS_NOT_BRIGHT_ENOUGH = (AMBIENT_LIGHT < VALUE); // TODO change the name of this variable to better reflect its purpose
-    return (LIGHT_WAS_NOT_BRIGHT_ENOUGH); // TODO change the name of this variable to better reflect its purpose
+    return (LIGHT_WAS_NOT_BRIGHT_ENOUGH);
 }
 
-
 bool Sensors::rockDetected() {
-  const int MESSURED_INFRARED_VALUE = analogRead(infraredPin);
+  const int MESSURED_INFRARED_VALUE = analogRead(INFRA_RED_SENSOR_GPIO_PIN);
   const bool ROCK_DETECTED = (MESSURED_INFRARED_VALUE > infraredThreshold);
   return ROCK_DETECTED;
 }
 
 bool Sensors::boundaryDetected() {
-  const int LEFT_LIGHT_VALUE = analogRead(leftLightPin);
-  const int RIGHT_LIGHT_VALUE = analogRead(rightLightPin);
+  const int LEFT_LIGHT_VALUE = (analogRead(LEFT_LIGHT_SENSOR_GPIO_PIN));
+  const int RIGHT_LIGHT_VALUE = (analogRead(RIGHT_LIGHT_SENSOR_GPIO_PIN));
   
-  const bool LEFT_SUSPECTS_BOUNDRY = (LEFT_LIGHT_VALUE > lightThreshold);
-  const bool RIGHT_SUSPECTS_BOUNDRY = (RIGHT_LIGHT_VALUE > lightThreshold);
+  const bool LEFT_SUSPECTS_BOUNDRY = (LEFT_LIGHT_VALUE > LIGHT_THRESHOLD);
+  const bool RIGHT_SUSPECTS_BOUNDRY = (RIGHT_LIGHT_VALUE > LIGHT_THRESHOLD);
   
   const bool BOUNDRY_DETECTED = (LEFT_SUSPECTS_BOUNDRY && RIGHT_SUSPECTS_BOUNDRY); 
   return BOUNDRY_DETECTED;
 }
 
 bool Sensors::cliffDetected() {
-  const long MINIMUM = (0); // TODO change the name of this variable to better reflect its purpose
-  const long MAXIMUM = (10); // TODO change the name of this variable to better reflect its purpose
-  const long DISTANCE = sonar.ping_cm();
+  const long ZERO_CENTIMETERS = (0);
+  const long TEN_CENTIMETERS = (10);
+  const long DISTANCE = (SONAR.ping_cm());
+    
+  const bool DISTANCE_IS_SHORTER_THEN_TEN_CENTIMETERS = (DISTANCE <= TEN_CENTIMETERS); 
+  const bool DISTANCE_IS_FURTHER_THEN_ZERO_CENTIMETERS = (DISTANCE > ZERO_CENTIMETERS);
   
-  return ((DISTANCE > MINIMUM) && (DISTANCE <= MAXIMUM)); // TODO break this down into separate boolean statements, that explain what this range means
+  const bool DISTANCE_IS_BETWEEN_ZERO_AND_TEN_CENTIMETERS = (
+    DISTANCE_IS_SHORTER_THEN_TEN_CENTIMETERS && DISTANCE_IS_FURTHER_THEN_ZERO_CENTIMETERS
+  );
+  
+  return (DISTANCE_IS_IN_RANGE); 
 }
 
 bool Sensors::hillDetected() {
-  const long MINIMUM = (10); // TODO change the name of this variable to better reflect its purpose
-  const long MAXIMUM = (20); // TODO change the name of this variable to better reflect its purpose
-  const long DISTANCE = sonar.ping_cm();
+  const long TEN_CENTIMETERS = (10);
+  const long TWENTY_CENTIMETERS = (20);
+  const long DISTANCE = (SONAR.ping_cm());
   
-  const bool DISTANCE_IS_BELOW_MAXIMUM = (DISTANCE <= MAXIMUM); // TODO change the name of this variable to better reflect its purpose
-  const bool DISTANCE_IS_ABOVE_MINIMUM = (DISTANCE > MINIMUM); // TODO change the name of this variable to better reflect its purpose
+  const bool DISTANCE_IS_SHORTER_THEN_TWENTY_CENTIMETERS = (DISTANCE <= MAXIMUM);
+  const bool DISTANCE_IS_FURTHER_THEN_TEN_CENTIMETERS = (DISTANCE > TEN_CENTIMETERS);
   
-  const bool DISTANCE_IS_IN_RANGE = (DISTANCE_IS_BELOW_MAXIMUM && DISTANCE_IS_ABOVE_MINIMUM);
+  const bool DISTANCE_IS_BETWEEN_TEN_AND_TWENTY_CENTIMETERS = (
+    DISTANCE_IS_SHORTER_THEN_TWENTY_CENTIMETERS && DISTANCE_IS_FURTHER_THEN_TEN_CENTIMETERS
+  );
+  
   return (DISTANCE_IS_IN_RANGE); 
 }
 
 float Sensors::getCompassHeading() {
-  compass.read();
-  const float HEADING = compass.getAzimuth();
+  COMPASS.read();
+  const float HEADING = COMPASS.getAzimuth();
   return HEADING;
 }
-
-// Sensors::~Sensors(){
-// If it does nothing, do not write it. It is just extra lines.
-// for now I have dumped it here
-// }
