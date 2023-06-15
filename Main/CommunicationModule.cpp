a9f797519a4995daa383a6583518398d6efa5217
Tygo van den Hurk <hazels.05.remount@icloud.com>
2023-06-15 15:45:05 +0200

    Quality-control on CommunicationModule.cpp

--- Main/CommunicationModule.cpp
+++ Main/CommunicationModule.cpp
@@ -3,81 +3,105 @@
 Communication::Communication(){
     WiFi.mode(WIFI_STA);
     if(BOARD) {
-      board = {0,0,0,0,0,0,0,0};
+      board = {0, 0, 0, 0, 0, 0, 0, 0};
     } else {
-      board = {0,0,0,0,0,0,0,0};
+      board = {0, 0, 0, 0, 0, 0, 0, 0};
     }
     senderSetup();
     receiverSetup();
 }
 
 Communication::~Communication(){
+  // If it does nothing, do not write it. It is just extra lines. 
+  // Plus I do not expect an arduino to destruct objects when shut
+  // down due to power loss. So is this even needed?
 }
 
 void Communication::onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  // this function will print if the packet was send succesfull or failed
-  // this is a callback  function
   printf("\r\nLast Packet Send Status:\t");
-  printf(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success\n" : "Delivery Fail\n");
+  
+  const bool AN_ERROR_OCCURED = (status == ESP_NOW_SEND_SUCCESS);
+  if (AN_ERROR_OCCURED) {
+    printf("Delivery Fail\n");
+    return;
+  }
+  
+  else {
+    printf("Delivery Success\n");
+  }
 }
 
 void Communication::senderSetup(){
-  // initating EPS-Now protocol. If it does not work an error message will pop up
-  if (esp_now_init() != ESP_OK) { 
-    //ESP_OK is a macro in the esp now libary to check if anything in the libary works
-    printf("Error %d ,please see the error list\n", errorCode);
+  /* initating EPS-Now protocol. */ {
+    const bool AN_ERROR_OCCURED = (esp_now_init() != ESP_OK);
+    if (AN_ERROR_OCCURED) { 
+      printf("Error %d ,please see the error list\n", errorCode);
+      return;
+    }
+  }
+
+  /* Calling back to the data sending function */ {
+    esp_now_register_send_cb(onDataSent);
+  }
+
+  /* Registering with a peer */ {
+    const bool DO_NO_ENCRYPT = (false);
+    const int CHANNLE = (0);
+    const int VALUE = (6); // TODO Rename to better discribe it's purpose.
     
-    return;
+    memcpy(peerInfo.peer_addr, broadcastAddress, VALUE);    
+    peerInfo.channel = CHANNLE;
+    peerInfo.encrypt = DO_NO_ENCRYPT;
   }
-  // Calling back to the data sending function
-  esp_now_register_send_cb(onDataSent);
 
-  // Registering with a peer
-  // ESP now is peer to peer, this system will be using one to many design
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-
-  //Add peer
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){ 
-    printf("Error %d Failed to add peer\n", errorCode);
-    return;
+  /* checking if adding peer was succesfull. */ {
+    const bool AN_ERROR_OCCURED = (esp_now_add_peer(&peerInfo) != ESP_OK);
+    if (AN_ERROR_OCCURED) { 
+      printf("Error %d Failed to add peer\n", errorCode);
+      return;
+    }
   }
-  errorCode++;
+  
+  errorCode++; // TODO Awnser question, if no error occured, why are we increasing the error code?
 }
 
 void Communication::receiverSetup(){
-    if (esp_now_init() != ESP_OK) { 
-        //ESP_OK is a macro in the esp now libary to check if anything in the libary works
+    const bool AN_ERROR_OCCURED = (esp_now_init() != ESP_OK);
+    if (AN_ERROR_OCCURED) {
         printf("Error %d ,please see the rror list\n", errorCode);
-        
         return;
-    } else{
-        printf("[CommunicationModule] ESPNow Init Success");
-        errorCode++;
     }
-    // Once ESPNow is successfully Init, we will register for recv CB to
-    // get recv packer info
-    esp_now_register_recv_cb(onReceive);
+  
+    printf("[CommunicationModule] ESPNow Init Success");
+    errorCode++; // TODO Awnser question, if no error occured, why are we increasing the error code?
+  
+    /* register for recv CB to get recv packer info */ {
+      esp_now_register_recv_cb(onReceive);
+    }
 }
 
 
 int Communication::send(sendMessage myData){
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-    // checking if it worked
-        if (result == ESP_OK) {
-          printf("[CommunicationModule]: Succes, message was send.\n");
-          return 0;
-          
-        }
-        else if (result != ESP_OK){
-          printf("[CommunicationModule]: Error %d, unable to send message\n", errorCode);
-          return 1;
-        } 
+  const int MESSAGE_SENT_SUCCESFULLY = (0);
+  const int ERROR = (1);
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  
+  /* checking if it worked */ {
+    const bool AN_ERROR_OCCURED = (result != ESP_OK);
+    if (AN_ERROR_OCCURED) {
+      printf("[CommunicationModule]: unable to send message, with error code Error %d. \n", errorCode);
+      return ERROR;
+    } 
+
+    else {
+      printf("[CommunicationModule]: Succes, message was send.\n");
+      return MESSAGE_SENT_SUCCESFULLY;
+    }
+  }
 }
 
-void Communication::onReceive(const uint8_t * mac, const uint8_t * incomingData, int len) {
-    memcpy(&myData, incomingData, sizeof(myData));
+void Communication::onReceive(const uint8_t * mac, const uint8_t * INCOMMING_DATA, int LENGTH) {
+    memcpy(&myData, INCOMMING_DATA, sizeof(myData));
     printf(myData.status);
 }
-    
